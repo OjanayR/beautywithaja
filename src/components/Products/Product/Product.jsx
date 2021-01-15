948ce247806334cb75cba12465c672b24a903c95
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-01-15 16:57:17 -0500

    adds styling components

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardAction, Typography, IconButton } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
@@ -10,7 +10,7 @@ const Product = ({ product }) => {
 
     return (
         <Card  className={classes.root}> 
-        <CardMedia className={classes.media} image='' title={Product.name} />
+        <CardMedia className={classes.media} image={product.image} title={Product.name} />
         <CardContent>
             <div className={classes.cardContent}>
                 <Typography variant='h5' gutterBottom>
@@ -20,7 +20,7 @@ const Product = ({ product }) => {
                     {product.price}
                 </Typography>
             </div>
-            <Typography variant='h2' color='textSecondary'>{product.description}</Typography>
+            <Typography variant='body2' color='textSecondary'>{product.description}</Typography>
         </CardContent>
         <CardActions disableSpacing className={classes.cardAction}>
             <IconButton aria-label="Add to Cart">
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,8 +4,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5' },
-    {id: 2, name: 'MacBook', description: 'Apple macbook.', price: '$10' },
+    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg?crop=1.00xw:0.789xh;0,0.150xh&resize=2048:*' },
+    {id: 2, name: 'MacBook', description: 'Apple macbook.', price: '$10', image: 'https://webobjects2.cdw.com/is/image/CDW/6013932?$product-main$' },
 
 ];
 
