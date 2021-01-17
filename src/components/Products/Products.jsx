2cf4b31f7151b9e365d64570d64980a71a41c3eb
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-01-16 20:01:12 -0500

    adds button functionality

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,13 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-/*const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg?crop=1.00xw:0.789xh;0,0.150xh&resize=2048:*' },
-    {id: 2, name: 'MacBook', description: 'Apple macbook.', price: '$10', image: 'https://webobjects2.cdw.com/is/image/CDW/6013932?$product-main$' },
-
-];*/
-
-const Products = ({ products }) => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
 
     return (
@@ -19,7 +13,7 @@ const Products = ({ products }) => {
         <Grid container justify= "center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product} />
+                    <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
             ))}
         </Grid>
