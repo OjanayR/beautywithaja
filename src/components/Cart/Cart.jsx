f2c095e7ac1790f3ce8938a303dae50c4152f4fe
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-01-17 01:31:30 -0500

    adds update/remove function

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 /*import classes from '*.module.css';*/
-
 import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
-const Cart = ({ cart }) => {
-    const isEmpty = !cart.line_items.length;
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
-        <Typography variant ="subtitle1">Looks like your shopping cart is empty, lets start shopping!</Typography>
+        <Typography variant ="subtitle1">Looks like your shopping cart is empty, lets start shopping!
+            <Link to="/" className={classes.link}>lets start shopping</Link>!
+        </Typography>
     );
 
     const FilledCart = () => (
@@ -17,26 +19,27 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{item.name}</div>
+                        <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} />
                     </Grid>
                 ))}
             </Grid>
             <div className={classes.cardDetails}>
                 <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
-                <di>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+                <div>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty Cart</Button>
                     <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
-                </di>
+                </div>
             </div>
         </>
+    );
 
-    )
+    if(!cart.line_items) return 'Loading...';
 
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant="h3"> Your Beauty With Aja Shopping Cart</Typography>
-            { isEmpty ? <EmptyCart /> : <FilledCart />}
+            <Typography className={classes.title} variant="h3" gutterBottom> Your 'Beauty With Aja' Shopping Cart</Typography>
+            { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
 }
