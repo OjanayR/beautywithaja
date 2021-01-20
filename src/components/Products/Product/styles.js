586389afe9b15d4efc15d17ee55e3785502c7aa1
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-01-19 19:40:44 -0500

    adds finishing touches to Navbar

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles (() => ({
     root: {
-        maxWidth: '100%'
+        maxWidth: '100%',
     },
     media: {
         height: 0,
@@ -15,6 +15,8 @@ export default makeStyles (() => ({
       cardContent: {
         display: 'flex',
         justifyContent: 'space-between',
+        alignItems: 'center',
+        alighContent: 'center',
       },
 
 }));
